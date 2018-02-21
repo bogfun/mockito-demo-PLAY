76761ad35b4435f3e8af6ad8dc1707f110489335
Bogdan.Chicos <Bogdan.Chicos@vauban.ro>
2018-02-21 17:46:37 +0200

    Some simple start for the exemplification of the need of mockito.

---
+++ src/test/java/com/in28minutes/mockito/mockitodemo/SomeBusinessTest.java
@@ -0,0 +1,23 @@
+package com.in28minutes.mockito.mockitodemo;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class SomeBusinessTest {
+
+	@Test
+	public void testFindTheGreatestFromAllData() {
+		SomeBusinessImpl business = new SomeBusinessImpl(new DataServiceStub());
+		assertEquals(24, business.findTheGreatestFromAllData());
+	}
+
+}
+
+class DataServiceStub implements DataService {
+	
+	@Override
+	public int[] retrieveAllData() {
+		return new int[]{24, 6, 15};
+	}
+}
